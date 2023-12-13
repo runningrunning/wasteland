bcd5bea1aec59acac5c471786acc202d7d93ecbe
yongc <yongc.running@gmail.com>
2023-12-13 18:02:39 +0100

    AdventOfCode 2023 12.5 :P.

--- adventofcode/2023/12.ts
+++ adventofcode/2023/12.ts
@@ -28,7 +28,7 @@ function process(input: string): number {
 
     let matched = 0;
 
-    // use binary to simulate the different
+    // use binary to stimulate the different
     for (let i = 0; i < Math.pow(2, count); i++) {
         let current = i;
         let pre = -1;
@@ -61,6 +61,119 @@ function process(input: string): number {
     return matched;
 }
 
+const FOLD = 5;
+let memory: Record<string, number> = {};
+
+function countMatch(myth: string, records: Array<number>, allQuestionToDamage = false): number {
+    const key = `${myth}-${records.join(',')}-${allQuestionToDamage ? 'true' : 'false'}`;
+    if (memory[key] !== undefined) {
+        return memory[key];
+    }
+
+    if (records.length === 0) {
+        if (myth.includes('#') || (allQuestionToDamage && myth.includes('?'))) {
+            memory[key] = 0;
+        } else {
+            memory[key] = 1;
+        }
+        return memory[key];
+    }
+
+    const needed = records.reduce((a, b) => a + b, 0) + records.length - 1;
+    if (myth.length < needed) {
+        memory[key] = 0;
+        return 0;
+    }
+
+    let count = 0;
+    if (records.length === 1) {
+        // check the continue '#'
+        for (let i = 0; i < myth.length - records[0] + 1; i++) {
+            const pre = myth.slice(0, i);
+            const last = myth.slice(i + records[0]);
+            const middle = myth.slice(i, i + records[0]);
+
+            if (pre.includes('#') || (allQuestionToDamage && pre.includes('?'))) {
+                break;
+            }
+
+            if (last.includes('#') || (allQuestionToDamage && last.includes('?'))) {
+                continue;
+            }
+
+            if (middle.includes('.') === false) {
+                count++;
+            }
+        }
+        memory[key] = count;
+    } else if (allQuestionToDamage) {
+        let pre = -1;
+        const ones: Array<number> = [];
+        for (const c of myth) {
+            if (c === '.') {
+                if (pre > 0) {
+                    ones.push(pre);
+                    pre = -1;
+                }
+            } else {
+                if (pre < 0) {
+                    pre = 1;
+                } else {
+                    pre++;
+                }
+            }
+        }
+
+        if (pre > 0) {
+            ones.push(pre);
+        }
+
+        if (ones.join(',') === records.join(',')) {
+            memory[key] = count = 1;
+        }
+        return count;
+    } else {
+        // for each question mark, it could be '.' or '#'
+        // when it's '.', divide the myth into 2 parts, match each parts with different records.
+        // since the '?' is checked from left to right, the questions has been '.' and checked already in
+        // the previous part so it can only act as damaged which is indicated by allQuestionToDamage
+        for (let i = 0; i < myth.length; i++) {
+            if (myth[i] === '?') {
+                for (let j = 0; j <= records.length; j++) {
+                    const pre = countMatch(myth.slice(0, i), records.slice(0, j), true);
+                    if (pre === 1) {
+                        count += countMatch(myth.slice(i + 1), records.slice(j), allQuestionToDamage);
+                    }
+                }
+            }
+        }
+        // treat the whole inputs as previous part for the last '?' in the myth or no '?' in the myth
+        count += countMatch(myth, records, true);
+        memory[key] = count;
+    }
+
+    return memory[key];
+}
+
+function process2(input: string): number {
+    input = input.trim();
+    if (input.length === 0) {
+        return 0;
+    }
+
+    const [myth, counts] = input.split(' ').map((x) => x.trim());
+    const realMyth = new Array(FOLD).fill(myth).join('?');
+    const realCountsStrings = new Array(FOLD).fill(counts).join(',');
+    const records = realCountsStrings
+        .split(',')
+        .map((x) => x.trim())
+        .filter((x) => x.length)
+        .map(Number);
+    // clear the memory
+    memory = {};
+    return countMatch(realMyth, records);
+}
+
 async function processLineByLine(file: string) {
     try {
         const rl = createInterface({
@@ -68,12 +181,14 @@ async function processLineByLine(file: string) {
             crlfDelay: Infinity,
         });
         let total = 0;
+        let total2 = 0;
         rl.on('line', (line) => {
             total += process(line);
+            total2 += process2(line);
         });
-
         await once(rl, 'close');
         console.log('total', total);
+        console.log('total2', total2);
     } catch (err) {
         console.error(err);
     }
